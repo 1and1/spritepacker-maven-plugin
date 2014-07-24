950b75a12a710e8217385b6545789f3241478bd2
Simon Siegler <simon.siegler@1und1.de>
2014-07-24 08:42:24 +0200

    Extract creation fo converter into a separate method for further tests.

--- src/test/java/net/oneandone/maven/plugins/spritepacker/converters/AbstractTextConverterTest.java
+++ src/test/java/net/oneandone/maven/plugins/spritepacker/converters/AbstractTextConverterTest.java
@@ -4,11 +4,11 @@ import net.oneandone.maven.plugins.spritepacker.ImagePacking;
 import net.oneandone.maven.plugins.spritepacker.NamedImage;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
+import java.io.File;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -19,24 +19,26 @@ import static org.hamcrest.Matchers.is;
 public class AbstractTextConverterTest {
     @Rule
     public ErrorCollector errorCollector = new ErrorCollector();
-    private AbstractTextConverter abstractTextConverter;
 
-    @Before
-    public void before() throws Exception {
-        abstractTextConverter = new AbstractTextConverter(null, null) {
-
-            @Override
-            protected String createOutput(List<NamedImage> imageList, ImagePacking imagePacking, Log log) throws MojoExecutionException {
-                return null;  // TODO
-            }
-        };
-
-    }
+    private String output;
+    private File file;
+    private String type;
 
     @Test
     public void testIntToPixel() throws Exception {
+        AbstractTextConverter abstractTextConverter = createConverter();
+
         errorCollector.checkThat(abstractTextConverter.intToPixel(0), is("0"));
         errorCollector.checkThat(abstractTextConverter.intToPixel(2), is("2px"));
         errorCollector.checkThat(abstractTextConverter.intToPixel(-32), is("-32px"));
+    }
+
+    private AbstractTextConverter createConverter() {
+        return new AbstractTextConverter(file, type) {
+            @Override
+            protected String createOutput(List<NamedImage> imageList, ImagePacking imagePacking, Log log) throws MojoExecutionException {
+                return output;
+            }
+        };
     }
 }
