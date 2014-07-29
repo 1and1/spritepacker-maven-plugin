67a8ac57fe9f5dd4dd1e32c490841adbe6042471
Simon Siegler <simon.siegler@1und1.de>
2014-07-29 17:27:54 +0200

    Improve readability of tests.

--- src/test/java/net/oneandone/maven/plugins/spritepacker/converters/LessPackingConverterTest.java
+++ src/test/java/net/oneandone/maven/plugins/spritepacker/converters/LessPackingConverterTest.java
@@ -3,7 +3,6 @@ package net.oneandone.maven.plugins.spritepacker.converters;
 import net.oneandone.maven.plugins.spritepacker.ImagePacking;
 import net.oneandone.maven.plugins.spritepacker.NamedImage;
 import org.apache.maven.plugin.logging.Log;
-import org.codehaus.plexus.util.StringUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,18 +18,18 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.isIn;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -43,7 +42,7 @@ public class LessPackingConverterTest {
     @DataPoints
     public static final String[] names = { "", ".", "abc", "0815-test", "&9.test" };
 
-    public static final String NAMESPACE = "test";
+    private static final String NAMESPACE = "test";
     private static final int NUM_IMAGES = 500;
 
     @Rule
@@ -83,23 +82,15 @@ public class LessPackingConverterTest {
 
     @Test
     public void testOutputWithNamespace() throws Exception {
-        testOutput(NAMESPACE);
-    }
-
-    @Test
-    public void testOutputWithoutNamespace() throws Exception {
-        testOutput(null);
-    }
-
-    private void testOutput(String namespace) throws Exception {
-        LessPackingConverter converter = new LessPackingConverter(null, namespace);
+        LessPackingConverter converter = new LessPackingConverter(null, NAMESPACE);
 
         String output = converter.createOutput(imageList, packing, log);
 
-        Map<NamedImage, Point> positions = new HashMap<>(NUM_IMAGES);
-        List<NamedImage> sizes = new ArrayList<>(NUM_IMAGES);
+        Set<NamedImage> positions = new HashSet<>(NUM_IMAGES);
+        Set<NamedImage> sizes = new HashSet<>(NUM_IMAGES);
+
         boolean namespaceDefined = false;
-        boolean createDefined = false;
+        boolean isFirstMixIn = true;
         boolean namespaceEnded = false;
 
         try (Scanner scanner = new Scanner(output)) {
@@ -112,79 +103,68 @@ public class LessPackingConverterTest {
                     continue;
                 }
 
-                // # signifies namespace
-                if (firstChar == '#') {
-                    errorCollector.checkThat("Namespace definition is expected", namespace, not(isEmptyOrNullString()));
-                    errorCollector.checkThat("Namespace syntax is correct", trimmed, is("#" + namespace + "{"));
-                    errorCollector.checkThat("Namespace only defined once", namespaceDefined, is(false));
+                if (!namespaceDefined) {
+                    errorCollector.checkThat("Namespace is correctly defined", trimmed, is("#" + NAMESPACE + "{"));
                     namespaceDefined = true;
-                    continue;
-                }
-
-                // if no comment and no namespace -> start of mixin definitions
-
-                if (!createDefined) {
-                    errorCollector.checkThat("Namespace was already defined if specified", StringUtils.isNotEmpty(namespace), is(namespaceDefined));
-                    errorCollector.checkThat("Create mixin is first", trimmed, is(".create(@name){.pos(@name);.size(@name);}"));
-                    createDefined = true;
-                    continue;
-                }
-
-                // } signifies end of namespace
-                if (firstChar == '}') {
-                    errorCollector.checkThat("Namespace was specified", namespace, not(isEmptyOrNullString()));
-                    errorCollector.checkThat("Namespace was defined", namespaceDefined, is(true));
-                    errorCollector.checkThat("Namespace only ended once", namespaceEnded, is(false));
+                } else if (firstChar == '.') {
+                    errorCollector.checkThat("The namespace hasn't ended yet", namespaceEnded, is(false));
+                    if (isFirstMixIn) {
+                        errorCollector.checkThat("Create mixin is first", trimmed, is(".create(@name){.pos(@name);.size(@name);}"));
+                        isFirstMixIn = false;
+                    } else {
+                        errorCollector.checkThat("A valid position or size mixin was found", addNameToPositionMap(trimmed, positions) || addNameToSizeList(trimmed, sizes), is(true));
+                    }
+                } else {
+                    errorCollector.checkThat(firstChar, is('}'));
                     namespaceEnded = true;
-                    continue;
                 }
+            }
+        }
 
-                errorCollector.checkThat("The namespace hasn't ended yet", namespaceEnded, is(false));
+        errorCollector.checkThat(namespaceDefined, is(true));
+        errorCollector.checkThat(namespaceEnded, is(true));
 
-                errorCollector.checkThat("A valid position or size mixin was found", addNameToPositionMap(trimmed, positions) || addNameToSizeList(trimmed, sizes), is(true));
+        errorCollector.checkThat("The correct number of position mixins was defined", positions.size(), is(NUM_IMAGES));
+        errorCollector.checkThat("The correct number of size mixins was defined", sizes.size(), is(NUM_IMAGES));
+
+        errorCollector.checkThat("All image positions were defined once", imageList, everyItem(isIn(positions)));
+        errorCollector.checkThat("All image sizes were defined once", imageList, everyItem(isIn(sizes)));
+    }
+
+    @Test
+    public void testOutputWithoutNamespace() throws Exception {
+        LessPackingConverter converter = new LessPackingConverter(null, null);
+
+        String output = converter.createOutput(imageList, packing, log);
+
+        Set<NamedImage> positions = new HashSet<>(NUM_IMAGES);
+        Set<NamedImage> sizes = new HashSet<>(NUM_IMAGES);
+        boolean isFirstMixin = true;
+
+        try (Scanner scanner = new Scanner(output)) {
+            while (scanner.hasNextLine()) {
+                String trimmed = scanner.nextLine().trim();
+                char firstChar = trimmed.charAt(0);
+                errorCollector.checkThat(firstChar, not(isIn(new Character[] { '#', '}' })));
+
+                // skip over comments
+                if (firstChar == '.') {
+                    // if no comment and no namespace -> start of mixin definitions
+                    if (isFirstMixin) {
+                        errorCollector.checkThat("Create mixin is first", trimmed, is(".create(@name){.pos(@name);.size(@name);}"));
+                        isFirstMixin = false;
+                    } else {
+                        errorCollector.checkThat("A valid position or size mixin was found", addNameToPositionMap(trimmed, positions) || addNameToSizeList(trimmed, sizes), is(true));
+                    }
+                }
             }
         }
 
         errorCollector.checkThat("The correct number of position mixins was defined", positions.size(), is(NUM_IMAGES));
         errorCollector.checkThat("The correct number of size mixins was defined", sizes.size(), is(NUM_IMAGES));
 
-        errorCollector.checkThat("All image positions were defined once", imageList, everyItem(isIn(positions.keySet())));
+        errorCollector.checkThat("All image positions were defined once", imageList, everyItem(isIn(positions)));
         errorCollector.checkThat("All image sizes were defined once", imageList, everyItem(isIn(sizes)));
-    }
-
-    private boolean addNameToPositionMap(String trimmed, Map<NamedImage, Point> positions) {
-        if (trimmed.startsWith(".pos")) {
-            String name = getNameFromMixin(trimmed);
-            NamedImage image = imageMap.get(name);
-            errorCollector.checkThat("Valid image name was defined", image, is(notNullValue()));
-            Point position = packing.getPosition(image);
-            errorCollector.checkThat("Position mixin defined correctly", trimmed,
-                                     is(".pos(" + name + "){background-position:" +
-                                        AbstractTextConverter.intToPixel(-position.x) + " " + AbstractTextConverter.intToPixel(-position.y) + ";}"));
-            errorCollector.checkThat("Image position was not defined twice", positions.get(image), is(nullValue()));
-            positions.put(image, position);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean addNameToSizeList(String trimmed, List<NamedImage> sizes) {
-        if (trimmed.startsWith(".size")) {
-            String name = getNameFromMixin(trimmed);
-            NamedImage image = imageMap.get(name);
-            errorCollector.checkThat("Valid image name was defined", image, is(notNullValue()));
-            errorCollector.checkThat("Size mixin defined correctly", trimmed,
-                                     is(".size(" + name + "){width:"+AbstractTextConverter.intToPixel(image.getWidth())+
-                                        ";height:" + AbstractTextConverter.intToPixel(image.getHeight()) + ";}"));
-            errorCollector.checkThat("Image size was not defined twice", sizes.contains(image), is(false));
-            sizes.add(image);
-            return true;
-        }
-        return false;
-    }
-
-    private String getNameFromMixin(String mixin) {
-        return mixin.substring(mixin.indexOf('(')+1, mixin.indexOf(')'));
     }
 
     @Theory
@@ -200,6 +180,39 @@ public class LessPackingConverterTest {
         String output = new LessPackingConverter(null, null).createOutput(imageList, packing, log);
         errorCollector.checkThat(getNameFromMixin(output.substring(output.lastIndexOf('.'))),
                                  is(AbstractTextConverter.sanitize(name)));
+    }
+
+    private boolean addNameToPositionMap(String trimmed, Set<NamedImage> positions) {
+        if (trimmed.startsWith(".pos")) {
+            String name = getNameFromMixin(trimmed);
+            NamedImage image = imageMap.get(name);
+            errorCollector.checkThat("Valid image name was defined", image, is(notNullValue()));
+            Point position = packing.getPosition(image);
+            errorCollector.checkThat("Position mixin defined correctly", trimmed,
+                                     is(".pos(" + name + "){background-position:" +
+                                        AbstractTextConverter.intToPixel(-position.x) + " " + AbstractTextConverter.intToPixel(-position.y) + ";}"));
+            errorCollector.checkThat("Image position was not defined twice", positions.add(image), is(true));
+            return true;
+        }
+        return false;
+    }
+
+    private boolean addNameToSizeList(String trimmed, Set<NamedImage> sizes) {
+        if (trimmed.startsWith(".size")) {
+            String name = getNameFromMixin(trimmed);
+            NamedImage image = imageMap.get(name);
+            errorCollector.checkThat("Valid image name was defined", image, is(notNullValue()));
+            errorCollector.checkThat("Size mixin defined correctly", trimmed,
+                                     is(".size(" + name + "){width:"+AbstractTextConverter.intToPixel(image.getWidth())+
+                                        ";height:" + AbstractTextConverter.intToPixel(image.getHeight()) + ";}"));
+            errorCollector.checkThat("Image size was not defined twice", sizes.add(image), is(true));
+            return true;
+        }
+        return false;
+    }
+
+    private String getNameFromMixin(String mixin) {
+        return mixin.substring(mixin.indexOf('(')+1, mixin.indexOf(')'));
     }
 
 }
