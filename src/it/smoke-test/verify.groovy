0f0322d6f634bf674f8c181c724e054ce1eb0e7d
Simon Siegler <simon.siegler@1und1.de>
2014-07-31 16:45:49 +0200

    Add assertions for smoke test.

---
+++ src/it/smoke-test/verify.groovy
@@ -0,0 +1,37 @@
+import java.nio.charset.StandardCharsets
+import java.nio.file.*
+
+assert basedir != null;
+
+def imageDir = basedir.toPath().resolve(Paths.get("target", "images"));
+
+
+
+System.err.println( groovy.lang.GroovySystem.getVersion());
+
+assert Files.exists(imageDir);
+
+def referenceDir = basedir.toPath().resolve("references");
+
+["json", "less", "css", "png"].each {
+  def basename = "sprite.".concat(it);
+  def output = imageDir.resolve(basename).toFile();
+  def reference = referenceDir.resolve(basename).toFile();
+  assert output.exists();
+  if (it == "png") {
+    def filesAreEqual = output.getBytes() == reference.getBytes();
+    assert filesAreEqual : "png output differs";
+  } else {
+    def charset = StandardCharsets.UTF_8.toString()
+    reference.withReader(charset) { reader ->
+      def lineNumber = 1;
+      output.eachLine { outputLine ->
+        def referenceLine = reader.readLine();
+        assert referenceLine == outputLine : "Line " + lineNumber + " differs";
+        lineNumber++;
+      };
+    };
+  }
+};
+
+return true;
