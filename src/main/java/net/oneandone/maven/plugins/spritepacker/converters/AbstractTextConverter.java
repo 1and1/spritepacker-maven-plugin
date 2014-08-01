7ad4d92926a968d0a25d21e622a98a33c26398dc
Michael Klein <michaelklein27@gmail.com>
2014-08-01 15:23:40 +0200

    Updated to take into account if output strings already contain CRLF EOLs

--- src/main/java/net/oneandone/maven/plugins/spritepacker/converters/AbstractTextConverter.java
+++ src/main/java/net/oneandone/maven/plugins/spritepacker/converters/AbstractTextConverter.java
@@ -52,7 +52,7 @@ public abstract class AbstractTextConverter implements PackingConverter {
         log.info("Generating " + type + " output...");
 
         String output = createOutput(imageList, imagePacking, log);
-        output.replaceAll("\n", EOL);
+        output.replaceAll("\\r?\\n", EOL);
 
         try {
             log.info("Saving " + type + " to file " + file.toAbsolutePath());
